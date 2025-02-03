ae35540d39e5fe419554a2511b4df18cf2e00ff3
Saurabh Agarwal <saurabh.ksa@gmail.com>
2025-02-03 13:03:33 +0530

    initial azure commit

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 13:00:20 2025
+
+@author: saurabh.agarwal
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! Flask App Deployed on Azure with CI/CD 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
